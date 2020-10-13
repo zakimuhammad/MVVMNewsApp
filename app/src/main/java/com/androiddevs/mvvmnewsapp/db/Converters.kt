994c86f41254a77fb52446e9c127202c47d05f58
Zaki Mukhammad <zakimuhammad20@gmail.com>
2020-10-13 19:22:43 +0700

    first commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/Converters.kt
@@ -0,0 +1,14 @@
+package com.androiddevs.mvvmnewsapp.db
+
+import androidx.room.TypeConverter
+import com.androiddevs.mvvmnewsapp.models.Source
+
+class Converters {
+
+    @TypeConverter
+    fun fromSource(source: Source): String = source.name
+
+    @TypeConverter
+    fun toSource(name: String): Source =
+        Source(name, name)
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constant.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constant {
+    companion object {
+        const val API_KEY = "4f1381c25582436f8640bc229a2440cc"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
